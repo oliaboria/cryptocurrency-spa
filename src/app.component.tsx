0a8692728e9cf75109b968ca44be7db343ac4753
Olga Boris <olgaboris94@gmail.com>
2020-08-03 19:24:50 +0300

    input styling

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import './app.css';
-
 import Header from './components/header';
 import Routes from './routes';
 
